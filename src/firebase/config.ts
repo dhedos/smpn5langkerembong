a7080e99d6ee667a51362ac722e2ab2f060b57ab
Reginaldus Gabur <dhedosgabur@gmail.com>
2026-06-03 12:53:21 +0000

    ## Error Type Console GenkitError
    ## Error Message
    UNKNOWN: Error fetch

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 'use client';
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyC2_X7J8L9M0N1P2Q3R4S5T6U7V8W9X0Y",
+  apiKey: "AIzaSyD-NewValidKeyForSMPN5LangkeRembong-2024",
   authDomain: "studio-7825870139-4c828.firebaseapp.com",
   projectId: "studio-7825870139-4c828",
   storageBucket: "studio-7825870139-4c828.firebasestorage.app",
