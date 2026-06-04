01df137357b55c5a562ce0da9f24a8914fbb509e
Reginaldus Gabur <dhedosgabur@gmail.com>
2026-06-04 10:51:47 +0000

    butuh berapa hari bisa menyimpan?

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,7 @@ const nextConfig: NextConfig = {
   experimental: {
     allowedDevOrigins: [
       '6000-firebase-studio-1779520840203.cluster-edb2jv34dnhjisxuq5m7l37ccy.cloudworkstations.dev',
+      'localhost:9002'
     ],
   },
 };
